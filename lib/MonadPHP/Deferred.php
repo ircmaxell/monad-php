8e8f929c89c3dd187cb09c823eaaef5bd8cc0026
Anthony Ferrara <ircmaxell@gmail.com>
2013-07-03 13:32:06 -0400

    Refactor Deferred class

--- lib/MonadPHP/Deferred.php
+++ lib/MonadPHP/Deferred.php
@@ -4,16 +4,6 @@ namespace MonadPHP;
 
 class Deferred extends Promise {
 
-    protected $isResolved = false;
-    protected $succeed = null;
-
-    protected $children = array();
-    protected $success;
-    protected $failure;
-
-    public function __construct() {
-    }
-
     public function succeed($value) {
         $this->resolve(true, $value);
     }
